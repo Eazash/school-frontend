01d270c823ae4c2b2fc14c9604231553c7150738
Ezra Ashenafi <eazash22@gmail.com>
2021-12-04 10:12:49 +0300

    feat: create home/students page

--- pages/index.vue
+++ pages/index.vue
@@ -1,76 +1,102 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        
-      </v-card>
+  <v-row justify="center" align="center" class="fill-height">
+    <v-col>
       <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+        <v-card-title>Students
+           <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        :append-icon="icons.search"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
         </v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
+          <v-data-table :headers="headers" :items="students" :search="search">
+            <template #item.actions="{item}"><!-- eslint-disable-line -->
+              <v-btn icon><v-icon dense>{{icons.pencil}}</v-icon></v-btn>
+              <v-btn icon  :disabled="item.audio === null" @click="playAudio(item)">
+                <v-icon v-if="item.isPlaying" dense>{{icons.stop}}</v-icon>
+                <v-icon v-else dense>{{icons.play}}</v-icon>
+              </v-btn>
+            </template>
+          </v-data-table>
         </v-card-text>
         <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
+          <v-btn dark color="green" nuxt to="/add">Add Student<v-icon>{{icons.plus}}</v-icon></v-btn>
+          <v-btn dark color="primary" nuxt to="/print">Print Student List<v-icon>{{icons.print}}</v-icon></v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
   </v-row>
 </template>
+
+<script>
+import {mdiMagnify, mdiPencil, mdiPlay, mdiPlus, mdiPrinter, mdiStop} from '@mdi/js'
+export default {
+  async asyncData({$axios}){
+    const {data } = await $axios.get("http://localhost:8000/api/students")
+    return { students:data}
+  },
+  data(){
+    return {
+      audio: {},
+      search: "",
+      headers: [
+        {
+          text: "Student ID",
+          value: "id",
+          width: "20%"
+        },
+        {
+          text: "Full Name",
+          value: "name"
+        },
+        {
+          text: "Actions",
+          value: "actions",
+          align: "right"
+        }
+      ],
+      icons: {
+        pencil: mdiPencil,
+        play: mdiPlay,
+        stop: mdiStop,
+        plus: mdiPlus,
+        print: mdiPrinter,
+        search: mdiMagnify
+      }
+    }
+  },
+  computed:{
+    audioPlaying(){
+      return this.audio.duration > 0 && !this.audio.paused;
+    }
+  },
+  methods:{
+   async playAudio(student){
+      const {audio: audioName} = student;
+      const indexOfStudent = this.students.findIndex( s => s.id === student.id);
+    try {
+      if (student.isPlaying){
+        this.audio.pause()
+        student.isPlaying = false;
+        this.$set(this.students, indexOfStudent, student)
+        return
+      }
+      else if(this.audioPlaying ){
+        this.audio.pause();
+      }
+      student.isPlaying = true;
+      this.$set(this.students, indexOfStudent, student)
+      this.audio = new Audio(`http://localhost:8000/public/${audioName}`);
+      this.audio.load()
+      await this.audio.play();
+    } catch (error) {
+      console.log(error)
+    }
+    }
+  }
+}
+</script>
