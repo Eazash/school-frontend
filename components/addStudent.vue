e33159afdc03c42c34816fbdb5ba45dcf057c7ef
Ezra Ashenafi <eazash22@gmail.com>
2021-12-09 17:04:24 +0300

    chore: make dialog closable

--- components/addStudent.vue
+++ components/addStudent.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog max-width="500px">
+  <v-dialog v-model="dialog" max-width="500px">
     <template #activator="{on,attrs}">
       <v-btn dark color="green" v-bind="attrs" class="action-button" small v-on="on">Add<v-icon class="action-icon">{{icons.plus}}</v-icon></v-btn>
     </template>
@@ -9,11 +9,11 @@
       <v-expansion-panels accordion>
         <v-expansion-panel >
           <v-expansion-panel-header>Add Student</v-expansion-panel-header>
-          <v-expansion-panel-content><AddStudentForm /></v-expansion-panel-content>
+          <v-expansion-panel-content><AddStudentForm @close="dialog= false" /></v-expansion-panel-content>
         </v-expansion-panel>
         <v-expansion-panel>
           <v-expansion-panel-header>Batch Import Students</v-expansion-panel-header>
-          <v-expansion-panel-content><ImportStudentsForm /></v-expansion-panel-content>
+          <v-expansion-panel-content><ImportStudentsForm @close="dialog = false"/></v-expansion-panel-content>
         </v-expansion-panel>
       </v-expansion-panels>
     </v-card>
@@ -27,7 +27,8 @@ export default {
     return {
       icons: {
         plus: mdiPlus
-      }
+      },
+      dialog: false
     }
   }
 }
