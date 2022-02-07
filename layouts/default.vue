352ad711e057e67f111deb56a7ad4ebc5cf1a42e
Ezra Ashenafi <eazash22@gmail.com>
2022-02-08 00:39:44 +0300

    feat: add default scanner page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,60 +1,70 @@
 <template>
-  <v-app dark >
-    <v-app-bar
-      fixed
-      dense
-      app
-    >
+  <v-app dark>
+    <v-navigation-drawer app clipped>
+      <v-list dense nav>
+        <v-list-item router to="/">
+          <v-list-item-icon
+            ><v-icon>{{ icons.scan }}</v-icon></v-list-item-icon
+          >
+          <v-list-item-content>
+            <v-list-item-title>Scan</v-list-item-title>
+          </v-list-item-content></v-list-item
+        >
+        <v-list-item router to="/students">
+          <v-list-item-icon
+            ><v-icon>{{ icons.students }}</v-icon></v-list-item-icon
+          >
+
+          <v-list-item-content>
+            <v-list-item-title>Students</v-list-item-title>
+          </v-list-item-content></v-list-item
+        >
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar fixed clipped-left dense app>
       <v-toolbar-title v-text="title" />
       <v-spacer />
     </v-app-bar>
     <v-main>
       <v-container>
-        <Nuxt @notify.capture="openSnackbar"/>
+        <Nuxt @notify.capture="openSnackbar" />
       </v-container>
-      <NotificationSnackbar :activator.sync="snackbar" :notify="snackbarContent"/>
+      <NotificationSnackbar
+        :activator.sync="snackbar"
+        :notify="snackbarContent"
+      />
     </v-main>
-    <v-footer
-      :absolute="true"
-      app
-    >
+    <v-footer :absolute="true" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import { mdiAccountGroup, mdiBarcodeScan } from '@mdi/js'
 export default {
-  data () {
+  data() {
     return {
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
+      icons: {
+        scan: mdiBarcodeScan,
+        students: mdiAccountGroup,
+      },
       title: 'Greenland School',
       snackbar: false,
-      snackbarContent: {}
+      snackbarContent: {},
     }
   },
-  created(){
+  created() {
     this.$nuxt.$on('notify', (event) => {
-      this.snackbar = true;
-      this.snackbarContent = event;
+      this.snackbar = true
+      this.snackbarContent = event
     })
   },
   methods: {
     openSnackbar(event) {
-      console.log("heeeere")
+      console.log('heeeere')
       console.log(event)
-    }
-  }
+    },
+  },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,153 +1,34 @@
-<template>
-  <v-row justify="center" align="center" class="fill-height">
-    <v-col v-if="!print">
-      <v-card>
-        <v-card-title>Students
-        </v-card-title>
-        <div class="d-flex justify-start flex-column flex-sm-row flex-wrap px-3 py-1"> 
-          <AddStudent />
-          <v-btn dark color="primary" small class="action-button" @click.stop="dialog = true">Scan ID<v-icon class="action-icon">{{icons.scan}}</v-icon></v-btn>
-          <v-btn dark color="orange" small class="action-button" @click="printIDS()">Print IDs<v-icon>{{icons.print}}</v-icon></v-btn>
-
-          <v-spacer />  
-          
-          <v-text-field
-          id="search-bar"
-            v-model="search"
-            class="flex-grow-1"
-            :append-icon="icons.search"
-            label="Search"
-            single-line
-            hide-details
-          ></v-text-field>
-        </div>
-        <v-card-text>
-          <v-data-table :headers="headers" :items="students" :search="search" :items-per-page="5">
-            <template #item.actions="{item}"><!-- eslint-disable-line -->
-              <div :key="item.id">
-                <!-- <v-btn icon><v-icon dense>{{icons.pencil}}</v-icon></v-btn> -->
-                <v-btn icon  :disabled="item.audio === null" @click="playAudio(item)">
-                  <v-icon v-if="item.isPlaying" dense>{{icons.stop}}</v-icon>
-                  <v-icon v-else dense>{{icons.play}}</v-icon>
-                </v-btn>
-                <RecordDialog :student="item" />
-              </div>
-            </template>
-          </v-data-table>
-        </v-card-text>
-      </v-card>
+!<template>
+  <v-row justify="center" align="top">
+    <v-col>
+      <v-container>
+        <v-card min-height="50vh" max-height="80vh">
+          <v-card-title>Scan Student IDs</v-card-title>
+          <v-divider />
+          <v-card-text>
+          <v-container fluid>
+            <v-row justify="center" align="center"><v-col>Student will be displayed here</v-col></v-row>
+            <v-text-field v-model="scanInput" @keyup.enter="() => found= !found"></v-text-field>
+            <ScanResult v-if="found" :scanned-text="scanInput"/>
+          </v-container>
+          </v-card-text>
+        </v-card>
+      </v-container>
     </v-col>
-    <v-col v-else cols="12">
-      <print-content :students="students" />
-    </v-col>
-    <v-dialog v-model="dialog" max-width="500px">      
-      <Scanner v-if="dialog" @close="dialog = false"/>
-    </v-dialog>
   </v-row>
 </template>
 
 <script>
-import {mdiCamera, mdiMagnify, mdiPencil, mdiPlay, mdiPrinter, mdiStop} from '@mdi/js'
 export default {
-  async asyncData({$axios, $config}){
-    const {data } = await $axios.get(`${$config.apiURL}/api/students`)
-    return { students:data}
-  },
-  data(){
+  data() {
     return {
-      audio: {},
-      dialog: false,  
-      print: false,
-      search: "",
-      headers: [
-        {
-          text: "Student ID",
-          value: "id",
-          width: "20%"
-        },
-        {
-          text: "Full Name",
-          value: "name"
-        },
-        {
-          text: "Grade",
-          value: "grade",
-          width: "15%"
-        },
-        {
-          text: "Section",
-          value: "section",
-          width: "15%",
-        },
-        {
-          text: "Actions",
-          value: "actions",
-          align: "right",
-          width: "15%",
-        }
-      ],
-      icons: {
-        pencil: mdiPencil,
-        play: mdiPlay,
-        stop: mdiStop,
-        print: mdiPrinter,
-        search: mdiMagnify,
-        scan: mdiCamera,
-      }
-    }
-  },
-  async fetch(){
-    const {data } = await this.$axios.get(`${this.$config.apiURL}/api/students`)
-    this.students = data;
-  },
-  computed:{
-    audioPlaying(){
-      return this.audio.duration > 0 && !this.audio.paused;
-    }
-  },
-  mounted(){
-    this.$nuxt.$on("notify", () =>{
-      this.$nuxt.refresh();
-    })
-  },
-  methods:{
-   async playAudio(student){
-      const {audio: audioName} = student;
-      const indexOfStudent = this.students.findIndex( s => s.id === student.id);
-      try {
-        if (student.isPlaying){
-          this.audio.pause()
-          student.isPlaying = false;
-          this.$set(this.students, indexOfStudent, student)
-          return
-        }
-        else if(this.audioPlaying ){
-          this.audio.pause();
-        }
-        student.isPlaying = true;
-        this.$set(this.students, indexOfStudent, student)
-        this.audio = new Audio(`${this.$config.apiURL}/public/${audioName}`);
-        this.audio.load()
-        await this.audio.play();
-      } catch (error) {
-        console.log(error)
-      }
-    },
-    async printIDS(){
-      this.print=true;
-      await this.$nextTick();
-      window.print();
-      this.print=false
+      scanInput: "",
+      found: false
     }
   }
 }
 </script>
 
 <style>
-#search-bar {
-  max-width: 450px;
-}
-.v-text-field {
-  padding: 0;
-}
+
 </style>
