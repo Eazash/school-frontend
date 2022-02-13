27322c9b27fb0a01e3723789d25f4bfbab279d75
Ezra Ashenafi <eazash22@gmail.com>
2022-02-13 09:40:17 +0300

    chore: refactor scan result

--- components/scanResult.vue
+++ components/scanResult.vue
@@ -1,65 +1,27 @@
 <template>
   <v-card class="ma-auto px-2 py-auto" width="100%" min-height="100px" outlined>
-    <template v-if="error">
-      <v-container>
-        <v-alert type="error" class="ma-auto">{{ errorMessage }}</v-alert>
-      </v-container>
-    </template>
-    <template v-if="found">
-      <StudentCard :student="student" />
-      <audio
-        v-if="student.audio !== null"
-        id="audio"
-        ref="audio"
-        :src="`${$config.apiURL}/public/${student.audio}`"
-        controls
-        autoplay
-      />
-      <v-alert v-else type="warning" outlined
-        >Student has no audio Recorded</v-alert
-      >
-    </template>
+    <StudentCard :student="student" />
+    <audio
+      v-if="student.audio !== null"
+      id="audio"
+      ref="audio"
+      :src="`${$config.apiURL}/public/${student.audio}`"
+      controls
+      autoplay
+    />
+    <v-alert v-else type="warning" outlined
+      >Student has no audio Recorded</v-alert
+    >
   </v-card>
 </template>
 
 <script>
 export default {
   props: {
-    scannedText: {
-      type: String,
+    student: {
+      type: Object,
       required: true,
     },
-  },
-  data() {
-    return {
-      error: false,
-      errorMessage: '',
-      found: false,
-      student: {},
-    }
-  },
-  async mounted() {
-    try {
-      const { data: student } = await this.$axios.get(
-        `${this.$config.apiURL}/api/students/${this.scannedText}`
-      )
-      this.student = student
-      this.$emit('complete')
-      this.found = true
-    } catch (error) {
-      this.$emit('complete')
-      if (error.response) {
-        if (error.response.status === 404) {
-          this.error = true
-          this.errorMessage = 'Invalid ID'
-        }
-      }
-      console.dir(error)
-      console.log(error.toJSON())
-    }
-  },
-  beforeUpdate() {
-    this.$refs.audio.stop()
   },
 }
 </script>
--- components/studentCard.vue
+++ components/studentCard.vue
@@ -1,23 +1,34 @@
 <template>
-  <v-row justify="space-between" align="center"
-    ><v-col cols=3>
-      <v-avatar :tile="student.image" size="150">
-        <v-img
-          v-if="student.image"
-          :src="`${$config.apiURL}/public/${student.image}`"
-        ></v-img>
-        <v-icon v-else>{{icons.account}}</v-icon>
-      </v-avatar>
-    </v-col>
-    <v-col><v-card flat class="student-card">
-          <v-card-title>{{student.name}}</v-card-title>
+  <div class="px-5">
+    <div class="my-2"><h2>Greenland School</h2></div>
+    <v-divider></v-divider>
+    <v-row justify="space-between" align="center" no-gutters>
+      <v-col cols="4">
+        <img src="logo.png" width="100" alt="" srcset="" />
+      </v-col>
+      <v-col cols="4">
+        <v-avatar :tile="student.image" size="100" left>
+          <v-img
+            v-if="student.image"
+            :src="`${$config.apiURL}/public/${student.image}`"
+          ></v-img>
+          <v-icon v-else>{{ icons.account }}</v-icon>
+        </v-avatar>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col
+        ><v-card flat class="student-card">
+          <v-card-title>{{ student.name }}</v-card-title>
           <v-card-subtitle>
-            {{student.id}}
+            {{ student.id }}
             <br />
-            {{student.grade}} | {{student.section}}
-            </v-card-subtitle>
-        </v-card></v-col>
-  </v-row>
+            {{ student.grade }} | {{ student.section }}
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -27,13 +38,13 @@ export default {
     student: {
       type: Object,
       required: true,
-    }
+    },
   },
   data() {
     return {
       icons: {
-        account: mdiAccount
-      }
+        account: mdiAccount,
+      },
     }
   },
 }
