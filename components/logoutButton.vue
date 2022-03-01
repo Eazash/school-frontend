5f6b373a3fc7360aa97c8711edc9651b6ff085cd
Ezra Ashenafi <eazash22@gmail.com>
2022-03-02 00:59:00 +0300

    fix: resolve login icon bug

--- components/logoutButton.vue
+++ components/logoutButton.vue
@@ -1,12 +1,20 @@
 <template>
-  <v-btn text outline color="primary" @click="logout">
+  <v-btn text outlined color="primary" class="mx-2" @click="logout">
     <v-icon>{{ icons.exit }}</v-icon>
     <span class="mr-1">Logout</span>
   </v-btn>
 </template>
 
 <script>
+import { mdiExitToApp } from '@mdi/js'
 export default {
+  data() {
+    return {
+      icons: {
+        exit: mdiExitToApp,
+      },
+    }
+  },
   methods: {
     logout() {
       this.$store.commit('logout')
