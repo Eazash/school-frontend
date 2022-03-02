99c466c0f5b89a644fca975b0a7c938cec90f212
Ezra Ashenafi <eazash22@gmail.com>
2022-03-02 06:33:32 +0300

    chore: bind api url to IP when locally run

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,87 +1,90 @@
 import colors from 'vuetify/es5/util/colors'
-export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    titleTemplate: '%s - school-frontend',
-    title: 'school-frontend',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+import {internalIpV4} from 'internal-ip'
+export default  async function () {
+  const API_URL = process.env.API_URL || `http://${await internalIpV4()}:8000`;
+  return {
+    head: {
+      titleTemplate: '%s - school-frontend',
+      title: 'school-frontend',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: ['@/assets/css/main.css'],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+      // https://go.nuxtjs.dev/eslint
+      '@nuxtjs/eslint-module',
+      // https://go.nuxtjs.dev/vuetify
+      '@nuxtjs/vuetify',
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/css/main.css'],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+      // https://go.nuxtjs.dev/axios
+      '@nuxtjs/axios',
+      // https://go.nuxtjs.dev/pwa
+      '@nuxtjs/pwa',
+      // Nuxt Socket
+      'nuxt-socket-io',
+    ],
+    io: {
+      server: {
+        cors: {
+          origin: '*'
+        }
+      },
+      sockets: [{ name: 'main', url: process.env.API_URL }]
+    },
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {},
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+    // PWA module configuration: https://go.nuxtjs.dev/pwa
+    pwa: {
+      manifest: {
+        lang: 'en'
+      }
+    },
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+    vuetify: {
+      icons: { iconfont: 'mdiSvg' },
+      customVariables: ['~/assets/variables.scss'],
+      theme: {
+        themes: {
+          dark: {
+            primary: colors.blue.darken2,
+            accent: colors.grey.darken3,
+            secondary: colors.amber.darken3,
+            info: colors.teal.lighten1,
+            warning: colors.amber.base,
+            error: colors.deepOrange.accent4,
+            success: colors.green.accent3
+          }
+        }
+      }
+    },
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    // Nuxt Socket
-    'nuxt-socket-io',
-  ],
-  io: {
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
     server: {
-      cors: {
-        origin: '*',
-      },
+      host: '0.0.0.0'
     },
-    sockets: [{ name: 'main', url: process.env.API_URL }],
-  },
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
-
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
-  pwa: {
-    manifest: {
-      lang: 'en',
-    },
-  },
-
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-  vuetify: {
-    icons: { iconfont: 'mdiSvg' },
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
-    },
-  },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
-  server: {
-    host: '0.0.0.0',
-  },
-  publicRuntimeConfig: {
-    apiURL: process.env.API_URL,
-  },
+    publicRuntimeConfig: {
+      apiURL: API_URL
+    }
+  };
 }
